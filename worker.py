f2ab5ff58129646875f703636009f5db0d47d4ec
Mike Meyer <mwm@mired.org>
2012-10-30 18:02:06 -0500

    Fix the worker so it at least loads.

--- worker.py
+++ worker.py
@@ -2,12 +2,15 @@
 
 """The text player app - gets moves from rq and texts the results back."""
 
+from os import getenv
+
 import redis
-from rq import worker, Queue, Connection
+from rq import Worker, Queue, Connection
+
 
 # Preload the game
 import pasture
 
-conn = redis.from_url(os.getenv('REDISTTOGO_URL', 'redis://localhost:6379'))
+conn = redis.from_url(getenv('REDISTTOGO_URL', 'redis://localhost:6379'))
 with Connection(conn):
     Worker(map(Queue, ('high', 'default', 'low'))).work()
