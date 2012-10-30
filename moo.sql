ad5650e99ba9a7dc572477a41008371feb38679f
Mike Meyer <mwm@mired.org>
2012-10-30 13:02:21 -0500

    Update the game mechanisms to use SQL to record the game. Add DDL for games.

---
+++ moo.sql
@@ -0,0 +1,25 @@
+drop table games cascade ;
+create table games (
+       id		serial primary key ,
+       goal_code	char(4) not null,
+       last_move	char(4) ,
+       move_count	smallint default 0,
+       solved		boolean
+       ) ;
+       
+drop table users ;
+create table users (
+       phone_number	text primary key ,
+       current_game	smallint references games (id) ,
+       validation_code	smallint
+       ) ;
+
+drop table moves ;
+create table moves (
+       game		integer references games (id) ,
+       move_count	smallint ,       
+       move		char(4) ,
+       bulls		smallint ,
+       cows		smallint
+       ) ;
+       
--- pasture.py
+++ pasture.py
@@ -4,10 +4,83 @@
 
 from random import sample
 
+from psycopg2 import connect
+
+class User(object):
+    def __init__(self, user):
+        self.db = connect("dbname=d95msa704d8g84 host=ec2-23-21-170-190.compute-1.amazonaws.com port=5432 user=jnisjfuuxcjlke password=0Mo84lR_s0te6YKVdfgTmLj18F sslmode=require")
+        self.db.autocommit = True
+        self.user = user
+        self.cur = self.db.cursor()
+        self.cur.execute(
+            'select current_game from users where phone_number = %s',
+            (user,))
+        data = self.cur.fetchone()
+        if data:
+            self.game = data[0]
+        else:
+            self.cur.execute('insert into users (phone_number) values (%s)',
+                             (user,))
+            self.game = None
+
+    def get_game(self):
+        game = Game(self)
+        if self.game != game.id:
+            self.cur.execute('update users set current_game = %s where phone_number = %s',
+                             (game.id, self.user))
+        return game
+
+    def __del__(self):
+        self.db.close()
+
 class Game(object):
-    def __init__(self):
+    def __init__(self, user):
+        self.user = user
+        self.cur = user.db.cursor()
+        self.id = user.game
+        if not self.id:
+            self.newgame()
+        else:
+            self.cur.execute('select goal_code, move_count, last_move from games where id = %s',
+                             (self.id,))
+            self.goal, self.move, self.last = self.cur.fetchone()
+
+    def __del__(self):
+        self.cur.close()
+
+    def newgame(self):
         self.goal = ''.join(chr(x) for x in sample(range(ord('0'), ord('9')), 4))
         self.move = 0
+        self.last = None
+        self.cur.execute('insert into games (goal_code) values (%s) returning id',
+                         (self.goal,))
+        self.id = self.cur.fetchone()[0]
+
+    def command(self, move):
+        method = getattr(self, 'do_' + move)
+        if method:
+            method()
+        elif move:
+            self.do_move(move)
+        else:
+            self.do_help()
+
+    def do_help(self):
+        return "This is Bulls and Cows. I picked a 4 unique digits (0-9). You need to guess them, in order. Text me your guess, and I'll tell you how many bulls (right digit in the right place) and cows (a digit I have, in the wrong place) you got."
+
+    def do_move(self, move):
+        bulls, cows = self.checkmove(move)
+        solved = bulls == 4
+        if move != self.last:
+            self.move += 1
+            self.cur.execute('insert into moves (game, move_count, move, bulls, cows, solved) values (%s, %s, %s, %s, %s, %s)',
+                             (self.id, self.move, move, bulls, cows, solved))
+            self.cur.execute('update games set move_count=%s, last_move=%s, solve=%s where id=%s',
+                             (self.move, move, solved, self.id))
+        if solved:
+            return "You won in {} moves".format(self.move)
+        else:
+            return "Move {}: {} Bulls, {} Cows".format(self.move, bulls, cows)
 
     def checkmove(self, move):
         bulls = cows = 0
@@ -17,6 +90,5 @@ class Game(object):
                     bulls += 1
                 else:
                     cows += 1
-        self.move += 1
-        return self.move, bulls, cows
+        return bulls, cows
 
