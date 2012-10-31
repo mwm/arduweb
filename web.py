d90a1e368739480e0e8f872b2d818568cc23b3b0
Mike Meyer <mwm@mired.org>
2012-10-31 06:43:40 -0500

    Fix the favicon file name

--- web.py
+++ web.py
@@ -13,9 +13,8 @@ def help():
 
 @app.route('/favicon.ico')
 def favicon():
-    return redirect(url_for('static', filename='favicon.ico'))
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'cow.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/<user>/<command>')
 def move(user, command):
