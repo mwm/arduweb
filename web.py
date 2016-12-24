e7417df43fb3357e86b919aba250c1c78cf454c5
Mike Meyer <mwm@mired.org>
2016-12-23 22:37:46 -0500

    Add a placeholder root page & some redirects

--- web.py
+++ web.py
@@ -1,9 +1,17 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, redirect
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return ('Arduboy stuff at ' + url_for('static', filename='repo.json') + '\n'
-            + url_for('static', filename='image.png') + '\n'
-            + url_for('static', filename='Tiny-1010.hex'))
+    return """<html><head><title>mwm's arduboy placeholder</title></head>
+              <body><p>You probably want <a href="/repo">the repo</a></p></body>"""
+
+@app.route('/repo')
+def repo():
+    return redirect(url_for('static', filename='repo.json'))
+
+@app.route('/family')
+def family():
+    return redirect(url_for('static', filename='family.json'))
