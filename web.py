ef19e0a7ebb232bf3a586c02926f6f124043ddb6
Mike Meyer <mwm@mired.org>
2017-01-13 15:07:07 -0500

    commit the refactored version for arduboy stuff.

--- web.py
+++ web.py
@@ -1,9 +1,12 @@
+"""The web server, via flask."""
+
+from makers import make_repo, make_arduboy_json
+from games import repos as repositories
+
 from flask import Flask, abort, redirect, url_for
 
-from repos import repos as repositories, make_repo, dumps
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -11,12 +14,19 @@ def hello_world():
               <body><p>You probably want <a href="/repo">the repo</a> or
               the <a href="/static/Tiny-1010.arduboy">arduboy file</a></p></body>"""
 
+@app.route('/arduboy/<game>')
+def arduboy(game):
+    out = make_arduboy_json(game)
+    if out is None:
+        abort(404)
+    return out
+
 @app.route('/repos/<repo>')
 def repos(repo):
-    if repo in repositories:
-        return make_repo(repo)
-    else:
+    out = make_repo(repo)
+    if out is None:
         abort(404)
+    return make_repo(repo)
 
 class do_repo_redirect(object):
     def __init__(self, repo):
@@ -26,4 +36,5 @@ class do_repo_redirect(object):
         return redirect(url_for('repos', repo=self.repo))
 
 for key in repositories.iterkeys():
-    app.add_url_rule('/' + key, key, do_repo_redirect(key))
+    if not key.startswith('_'):
+        app.add_url_rule('/' + key, key, do_repo_redirect(key))
